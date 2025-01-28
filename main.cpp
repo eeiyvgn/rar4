f0041e6b66ea36b4ec709244308782943611a6d9
Evgeniy <e.koskov@bk.ru>
2025-01-28 23:53:01 +0500

    add first structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
 
 using namespace std;
+
+#pragma pack(push, 1)
+
+struct VolumeHeaderFormat
+{
+    uint16_t header_crc;
+    uint8_t header_type;
+    uint16_t header_flags;
+    uint16_t header_size;
+};
 
 int main()
 {
